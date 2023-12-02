58b4d9e58084a91a46be024e87fe0000e4fcf148
colewiltse <cole.wiltse@mail.utoronto.ca>
2023-12-02 17:13:38 -0500

    adding very basic version of petpal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './pages/layout';
+import Login from './pages/login';
+import Blogs from './pages/blogs';
+import PetList from './pages/petpal_index';
+import NoPage from './pages/no_page';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PetList />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
