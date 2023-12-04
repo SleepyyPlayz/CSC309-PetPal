796e1b1124ff0e5c19379ec543cb87dae3286c49
colewiltse <cole.wiltse@mail.utoronto.ca>
2023-12-03 23:48:25 -0500

    Adding nice looking pet list index

--- backend/petpal_api/urls.py
+++ backend/petpal_api/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls import include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -47,3 +49,6 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- frontend/src/pages/petpal_index/index.jsx
+++ frontend/src/pages/petpal_index/index.jsx
@@ -69,19 +69,35 @@ const PetList = () => {
             </div>
           </section>
 
-          <div>
-            <h2>Pet List</h2>
-            <ul>
-              {pets.map(pet => (
-                <li key={pet.id}>{pet.pet_name}</li>
-              ))}
-            </ul>
-            {preNextPage && (
+          <div className="album py-1">
+            <div className="container">
+              <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                {pets.map(pet => (
+                  // <li key={pet.id}>{pet.pet_name}</li>
+                  <div className="col">
+                    <div className="card shadow-sm">
+                      <img className="card-img-top" width="100%" src={pet.pet_picture !== null ? `${pet.pet_picture}` : "/no_image.jpg" } alt="Pet Pic"/>
+                      <div className="card-body">
+                        <h5 className="card-title">{pet.pet_name}</h5>
+                        <p className="card-text">{pet.breed} - {pet.gender} - {pet.age} years old</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                          {/* <a className="btn btn-sm btn-outline-info me-3" href="pet-detail-page.html">Details</a> */}
+                          <small className="text-muted">{pet.shelter.name}</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+
+              </div>
+            </div>
+          </div>
+          
+          {preNextPage && (
               <button onClick={() => setNextPage(preNextPage)}>
                 Load More
               </button>
             )}
-          </div>
 
         </main>
       </body>
