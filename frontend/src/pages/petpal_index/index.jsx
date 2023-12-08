c2af40c334de9cd344e5a185d0adce21660134a7
colewiltse <cole.wiltse@mail.utoronto.ca>
2023-12-08 17:30:39 -0500

    Adding search functionality to pet listings. Also adding search parameters being stored in url

--- frontend/src/pages/petpal_index/index.jsx
+++ frontend/src/pages/petpal_index/index.jsx
@@ -1,14 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
 
 
 const PetList = () => {
   console.log('Rendering PetList'); // Add this line
+  const navigate = useNavigate()
+
 
   const [pets, setPets] = useState([]);
   const [nextPage, setNextPage] = useState(null);
   const [currentPage, setCurrentPage] = useState(`http://127.0.0.1:8000/pet_listings/`);
   const [previousPage, setPreviousPage] = useState(null);
+  const [filterValues, setFilterValues] = useState({
+    shelter: '',
+    status: '',
+    breed: '',
+    species: '',
+    age: '',
+    ordering: '',
+  });
+  const [shelters, setShelters] = useState([]);
+  const [pageNum, setPageNum] = useState(1);
+  const [queryParams, setQueryParams] = useState('');
+
+  const handleFilterChange = (filter, value) => {
+    setFilterValues((prevValues) => ({
+      ...prevValues,
+      [filter]: value,
+    }));
+  };
+
+  useEffect(() => {
+    // Fetch the list of shelters
+    fetch('http://127.0.0.1:8000/accounts/shelters/?limit=100')  // Replace with your actual endpoint
+      .then((response) => response.json())
+      .then((data) => {
+        setShelters(data.results);
+      })
+      .catch((error) => {
+        console.error('Error fetching shelters:', error);
+      });
+  }, []);
+
 
 
   useEffect(() => {
@@ -36,10 +71,12 @@ const PetList = () => {
         } else {
           setPreviousPage(null);
         }
+        navigate(`?page=${pageNum}&${queryParams.toString()}`);
       })
       .catch(error => {
         console.error('Error fetching pets:', error);
       });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentPage]); // Run this effect when nextPage changes
 
   return (
@@ -74,6 +111,117 @@ const PetList = () => {
             </div>
           </section>
 
+          <div className="container">
+            <div className="row justify-content-center">
+                <form>
+                    <label className="form-label ms-3">
+                      Shelter:
+                      <select
+                        className="form-select mb-3"
+                        value={filterValues.shelter}
+                        onChange={(e) => handleFilterChange('shelter', e.target.value)}
+                      >
+                        <option value="">Select a shelter</option>
+                        {shelters.map((shelter) => (
+                          <option key={shelter.underlying_user} value={shelter.underlying_user}>
+                            {shelter.name}
+                          </option>
+                        ))}
+                      </select>
+                    </label>
+                  
+
+                  <label className="form-label ms-3">
+                    Status:
+                    <select
+                      className="form-select mb-3"
+                      value={filterValues.status}
+                      onChange={(e) => handleFilterChange('status', e.target.value)}
+                    >
+                      <option value="">All</option>
+                      <option value="available">Available</option>
+                      <option value="adopted">Adopted</option>
+                      <option value="pending">Pending</option>
+                      <option value="withdrawn">Withdrawn</option>
+
+                    </select>
+                  </label>
+
+                  <label className="form-label ms-3">
+                    Breed:
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={filterValues.breed}
+                      onChange={(e) => handleFilterChange('breed', e.target.value)}
+                    />
+                  </label>
+
+                  <label className="form-label ms-3">
+                    Species:
+                    <select
+                      className="form-select"
+                      value={filterValues.species}
+                      onChange={(e) => handleFilterChange('species', e.target.value)}
+                    >
+                      <option value="">All</option>
+                      <option value="dog">Dog</option>
+                      <option value="cat">Cat</option>
+                      <option value="bird">Bird</option>
+                      <option value="rabbit">Rabbit</option>
+                    </select>
+                  </label>
+
+                  <label className="form-label ms-3">
+                    Age:
+                    <input
+                      type="number"
+                      className="form-control"
+                      min="0"
+                      value={filterValues.age}
+                      onChange={(e) => handleFilterChange('age', e.target.value)}
+                    />
+                  </label>
+
+                  <label className="form-label ms-3">
+                    Ordering:
+                    <select
+                      className="form-select"
+                      value={filterValues.ordering}
+                      onChange={(e) => handleFilterChange('ordering', e.target.value)}
+                    >
+                      <option value="">None</option>
+                      <option value="age">Age</option>
+                      <option value="pet_name">Pet Name</option>
+                    </select>
+                  </label>
+
+
+
+                  {/* Add similar select elements for other filters (breed, age, ordering) */}
+                  
+                  <button
+                    className='ms-3'
+                    type="button"
+                    onClick={() => {
+                      const currentQueryParams = new URLSearchParams(filterValues);
+                      const newUrl = `http://127.0.0.1:8000/pet_listings/?${currentQueryParams.toString()}`;
+                      setQueryParams(currentQueryParams.toString());
+                      setPageNum(1);
+                      setCurrentPage(newUrl);
+                      console.log("just did the button");
+
+                    }}
+                  >
+                    Apply Filters
+                  </button>
+                </form>
+
+            </div>
+          </div>
+
+          
+
           <div className="album py-1">
             <div className="container">
               <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
@@ -100,16 +248,24 @@ const PetList = () => {
           </div>
           
           {nextPage && (
-              <button onClick={() => setCurrentPage(nextPage)}>
+              <button onClick={() => {
+                setPageNum(pageNum + 1);
+                setCurrentPage(nextPage);
+                }}>
                 Next Page
               </button>
             )}
           
           {previousPage && (
-              <button onClick={() => setCurrentPage(previousPage)}>
+              <button onClick={() => {
+                setPageNum(pageNum - 1);
+                setCurrentPage(previousPage);
+                }}>
                 Previous Page
               </button>
             )}
+
+            <p>{pageNum}</p>
 
         </main>
       </body>
