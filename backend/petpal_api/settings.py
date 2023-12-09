cf677081f6a697458bebfae4cfafda0f16efefd0
colewiltse <cole.wiltse@mail.utoronto.ca>
2023-12-08 22:06:06 -0500

    Changing pagination to page number pagination

--- backend/petpal_api/settings.py
+++ backend/petpal_api/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
 
     'DEFAULT_PERMISSION_CLASSES' : [
