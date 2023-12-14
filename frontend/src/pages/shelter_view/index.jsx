6071df8d01414806b7103cd9a9c2f6365b54db4b
SleepyyPlayz <16306416+SleepyyPlayz@users.noreply.github.com>
2023-12-14 02:05:32 -0500

    minor styling change for shelter detail view

--- frontend/src/pages/shelter_view/index.jsx
+++ frontend/src/pages/shelter_view/index.jsx
@@ -3,78 +3,72 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-function ShelterView( {isLoggedIn} ) {
-  const [pet, setPet] = useState(null);
-  const { id } = useParams();
-  var accessToken;
+import Container from 'react-bootstrap/Container';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from 'react-bootstrap/Button';
 
-  useEffect(() => {
-    if (isLoggedIn) {
-        accessToken = localStorage.getItem('access');
-    }
-    else {
-        window.location.href = '/login';
-    }
-    
-    const apiUrl = `http://127.0.0.1:8000/accounts/shelter/${id}/`
-    // axios.get(`http://localhost:8000/api/pets/${id}/`)
-    //   .then(response => setPet(response.data))
-    //   .catch(error => console.error('Error fetching data:', error));
-    console.log(accessToken);
+
+export default function ShelterView( {isLoggedIn} ) {
+    const [pet, setPet] = useState(null);
+    const { id } = useParams();
+    var accessToken;
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            accessToken = localStorage.getItem('access');
+        }
+        else {
+            window.location.href = '/login';
+        }
+        
+        const apiUrl = `http://127.0.0.1:8000/accounts/shelter/${id}/`
+        // axios.get(`http://localhost:8000/api/pets/${id}/`)
+        //   .then(response => setPet(response.data))
+        //   .catch(error => console.error('Error fetching data:', error));
+        console.log(accessToken);
         fetch(apiUrl, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${accessToken}`,
             },
         })
-      .then(response => response.json())
-      .then(data => setPet(data))
-      .catch(error => {
-        console.error('Error fetching pets:', error);
-      });
-  }, [id]);
+        .then(response => response.json())
+        .then(data => setPet(data))
+        .catch(error => {
+            console.error('Error fetching pets:', error);
+        });
+    }, [id]);
 
-  if (!pet) {
-    return <div>Loading...</div>;
-  }
+    if (!pet) {
+        return <div>Loading...</div>;
+    }
 
-  return (
-    // <div>
-    //   <h2>{pet.name}</h2>
-    //   <p>Species: {pet.species}</p>
-    //   <p>Age: {pet.age}</p>
-    // </div>
-    <>
-
-        <main>
-            <div className="container mx-auto align-items-center row row-cols-1 row-cols-md-2 g-4 mt-2 mb-1 mx-5">
-                <div className="col">
+    return (
+        <Container>
+            <Row xs={1} md={2} className="mx-auto align-items-top g-4 mt-2 mb-1 mx-5">
+                <Col>
                     <img src={pet.profile_picture!== null ? `${pet.profile_picture}` : "/no_image.jpg" } alt="Pet Pic" className="img-fluid card" />
-                </div>
+                </Col>
 
-                <div className="col">
-                    <div className="card" >
-                        <div className="card-body">
-                            <h1 className="card-title">{pet.name}</h1>
-                            <p className="card-text">{pet.address_line_1}</p>
-                            <p className="card-text">{pet.address_line_2}</p>
-                            <p className="card-text">
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>{pet.name}</Card.Title>
+                            <Card.Text>{pet.address_line_1}</Card.Text>
+                            <Card.Text>{pet.address_line_2}</Card.Text>
+                            <Card.Text>
                                 {/* <a href="shelter-detail-page.html">Toronto Pet Shelter</a> */}
                                 {pet.postal_code}
-                            </p>
+                            </Card.Text>
                       
-
                             {/* <a href="pet-adoption-form.html" className="btn btn-lg adopt-button">Adopt Me!</a> */}
-                        </div>
-                    </div>
-                </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
 
-            </div>
-        </main>
-    
-    
-    </>
-  );
+            </Row>
+        </Container>
+    );
 }
-
-export default ShelterView;
