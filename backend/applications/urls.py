d3940496a824916a0d81a4747010af16f1c05fea
angelo <huang.angelo@hotmail.ca>
2023-12-14 01:26:51 -0500

    my apps

--- backend/applications/urls.py
+++ backend/applications/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import NewPetApplication, FilledAppShelterView, FilledAppUserView, PetApplicationListView
+from .views import NewPetApplication, FilledAppShelterView, FilledAppUserView, PetApplicationListView, PetApplicationListViewUser
 
 urlpatterns = [
     path("", NewPetApplication.as_view(), name="new-pet-application"),
     path("filled-applications/user/<int:pk>/", FilledAppUserView.as_view(), name="filled-applications-user"),
     path("filled-applications/shelter/<int:pk>/", FilledAppShelterView.as_view(), name="filled-applications-shelter"),
-    path("filled-applications/shelter/list/", PetApplicationListView.as_view(), name="application-list")
+    path("filled-applications/shelter/list/", PetApplicationListView.as_view(), name="application-list"),
+    path("filled-applications/user/list/", PetApplicationListViewUser.as_view(), name="application-list")
 
 ]
 
